1a15cc14cd952168c3bce87a4614d8f1fa1d797b
junhui guo <guojunhui@h-ui.net>
2020-10-12 13:35:21 +0800

    jQuery.Huifold.js折叠增加默认展开的状态。

--- static/h-ui/js/H-ui.js
+++ static/h-ui/js/H-ui.js
@@ -1,10 +1,10 @@
 /*-----------H-ui前端框架-------------
-* H-ui.js v3.1.11
+* H-ui.min.js v3.2
 * http://www.h-ui.net/
 * Created & Modified by guojunhui
-* Date modified 2019.01.18
+* Date modified 2020.10.12
 *
-* Copyright 2013-2019 北京颖杰联创科技有限公司 All rights reserved.
+* Copyright 2013-2020 郭俊辉 All rights reserved.
 * Licensed under MIT license.
 * http://opensource.org/licenses/MIT
 */
@@ -4481,28 +4481,38 @@ function displaynavbar(obj){
 } (window.jQuery);
 
 /* =======================================================================
- * jQuery.Huifold.js v2.0 折叠
+ * jQuery.Huifold.js v2.1 折叠
  * http://www.h-ui.net/
  * Created & Modified by guojunhui
- * Date modified 2017.05.05
- *
- * Copyright 2017 北京颖杰联创科技有限公司 All rights reserved.
+ * Date modified 2012.10.12
+ * Copyright 2017-2020 郭俊辉 All rights reserved.
  * Licensed under MIT license.
  * http://opensource.org/licenses/MIT
  * ========================================================================*/
 !function($) {
 	$.fn.Huifold = function(options){
 		var defaults = {
-			titCell:'.item .Huifold-header',
-			mainCell:'.item .Huifold-body',
-			type: 1, //1	只打开一个，可以全部关闭;2	必须有一个打开;3	可打开多个
+			titCell:'.Huifold-header',
+			mainCell:'.Huifold-body',
+			type:1,//1	只打开一个，可以全部关闭;2	必须有一个打开;3	可打开多个
 			trigger:'click',
 			className:"selected",
-			speed:'first',
+			speed:'normal',
+			openKeys: []
 		}
 		var options = $.extend(defaults, options);
-		this.each(function(){
+		this.each(function() {
 			var that = $(this);
+			if(options.openKeys && options.openKeys.length > 0) {
+				for(var i=0;i<options.openKeys.length; i++) {
+					that.find(options.titCell).eq(options.openKeys[i]).addClass(options.className);
+					that.find(options.mainCell).eq(options.openKeys[i]).show();
+					if (that.find(options.titCell).eq(options.openKeys[i]).find("b")) {
+						that.find(options.titCell).eq(options.openKeys[i]).find("b").html("-");
+					}
+				}
+			}
+
 			that.find(options.titCell).on(options.trigger,function(){
 				if ($(this).next().is(":visible")) {
 					if (options.type == 2) {
@@ -4532,7 +4542,7 @@ function displaynavbar(obj){
 					}
 				}
 			});
-
+			
 		});
 	}
 } (window.jQuery);
